cfc3efab1dc90d8bf9e6f984ac07980b368b04fc
Yamazaki Mitsufumi <mitsufumi.yamazaki@outlook.jp>
2017-06-11 19:33:05 +0900

    add networkTransform
    This synchronize Transform smoothly.

--- Assets/MusicWar/Scripts/IInputProvider.cs
+++ Assets/MusicWar/Scripts/IInputProvider.cs
@@ -1,6 +1,5 @@
 ﻿using UnityEngine;
 using UniRx;
-using UnityEngine.Networking;
 
 public interface IInputProvider {
     IReadOnlyReactiveProperty<Vector3> Move { get; }
---
+++ Assets/MusicWar/Scripts/NetworkTransform.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UniRx;
+using UniRx.Triggers;
+
+public class NetworkTransform : NetworkBehaviour
+{
+    [SyncVar]
+    private Vector3 _syncPosition;
+
+	void Start ()
+    {
+        this.UpdateAsObservable()
+            .Where(_ => isLocalPlayer)
+            .Subscribe(_ => CmdSynchronizePosition(transform.position));
+
+        this.UpdateAsObservable()
+            .Where(_ => !isLocalPlayer)
+            .Subscribe(_ => transform.position += (_syncPosition - transform.position)*0.016f);
+
+    }
+
+    [Command]
+    void CmdSynchronizePosition(Vector3 position)
+    {
+        _syncPosition = position;
+    }
+
+}
